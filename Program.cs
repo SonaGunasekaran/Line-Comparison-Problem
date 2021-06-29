62bdebceac8f271bb78b342c4ad0d83eba75166d
[Sona] <[sona16061999@gmail.com]>
2021-06-29 12:06:43 +0530

    Welcome

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LineComparision
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program on Master Branch");
+        }
+    }
+}
